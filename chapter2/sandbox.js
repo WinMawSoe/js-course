5b0eec0d58d75c251c0fe6d7fa8c592624785d7e
WinMawSoe <100017341+WinMawSoe@users.noreply.github.com>
2022-06-01 22:06:04 +0630

    chapter2

---
+++ chapter2/sandbox.js
@@ -0,0 +1,13 @@
+let userName = "Win Maw";
+let homeTown = "Mohnyin";
+
+console.log(userName, homeTown);
+
+// userName= "Khin";
+// console.log(userName);
+
+// const points= 100;
+// console.log(points);
+
+var year = 2022;
+console.log(year);
