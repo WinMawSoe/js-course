62f7427b7549c4e9eef7548ad768011c7f39b254
WinMawSoe <100017341+WinMawSoe@users.noreply.github.com>
2022-06-03 20:41:29 +0630

    chapter 2

--- chapter2/sandbox.js
+++ chapter2/sandbox.js
@@ -1,13 +1,70 @@
-let userName = "Win Maw";
-let homeTown = "Mohnyin";
+let email = "winmawsoe11@gmail.com";
 
-console.log(userName, homeTown);
+// string concatenation
+// let firstName = "Win";
+// let lastName = "Maw";
 
-// userName= "Khin";
-// console.log(userName);
+// let fullName = firstName + " " + lastName;
+// console.log(fullName);
 
-// const points= 100;
-// console.log(points);
+// getting characters
+// console.log(fullName[2]);
 
-var year = 2022;
-console.log(year);
+//  string length
+// console.log(fullName.length);
+
+//  string methods
+// console.log(fullName.toUpperCase());
+// let result = fullName.toLowerCase();
+// console.log(result, fullName);
+
+// let index = email.indexOf("@");
+// console.log(index);
+
+// common string methods
+
+//let result = email.lastIndexOf("e");
+//let result = email.slice(1, 4);
+//let result = email.substring(5, 10);
+//let result = email.replace("w", "p");
+
+//let result = email.replace("o", "p");
+
+//console.log(result);
+
+let radius = 10;
+const pi = 3.14;
+
+//console.log(radius, pi);
+
+//math operations +, -, *, /, **, %
+//console.log(10/2);
+//let result = radius % 3;
+//let result = pi* radius**2;
+
+//order of operation - B I D M A S
+
+//let result= 5*(10-3)**2;
+
+//console.log(result);
+
+let likes = 10;
+
+//likes = likes +1;
+//likes++;
+//likes--;
+
+//likes+= 10;
+//likes-= 5;
+//likes*= 2;
+//likes/= 2;
+
+//console.log(likes);
+
+//NaN - not a number
+
+//console.log(5/ 'hello');
+//console.log(5 * 'hello');
+
+let result = "the blog has" + " " + likes + " " + "likes";
+console.log(result);
