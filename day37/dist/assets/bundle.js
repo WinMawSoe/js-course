d50fa184e9e57eae629eafb0e0e9f04d0c23f92d
WinMawSoe <100017341+WinMawSoe@users.noreply.github.com>
2022-07-15 16:39:20 +0630

    day37

---
+++ day37/dist/assets/bundle.js
@@ -0,0 +1,11 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+const greet = (name) => {
+  console.log(`hello ${name}`);
+};
+greet("mario");
+greet("luigi");
+greet("link");
+
+/******/ })()
+;
